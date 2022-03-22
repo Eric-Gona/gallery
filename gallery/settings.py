2770788c17b5ed9fcaec046a9856150b082542aa
Eric-Gona <eric.gona254@gmail.com>
2022-03-22 15:04:39 +0300

    adds images

--- gallery/settings.py
+++ gallery/settings.py
@@ -18,8 +18,8 @@ from decouple import config
 from django.db import DatabaseError
 import django_heroku
 import dj_database_url
+
 db_from_env = dj_database_url.config(conn_max_age=500)
-DatabaseError['default'].update(db_from_env)
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
